f3b0c28aab6d514ca2d4d289f86faf10038fbf06
Slokh <slokhx@gmail.com>
2024-11-27 14:34:05 -0500

    fix

--- packages/utils/src/proofs/verify.ts
+++ packages/utils/src/proofs/verify.ts
@@ -4,6 +4,13 @@ import { ProofData } from '@noir-lang/types'
 import { getCircuit } from './utils'
 import { ProofType } from './generate'
 
+export async function getProvingBackend(proofType: ProofType) {
+  const circuit = getCircuit(proofType)
+  const backend = new BarretenbergBackend(circuit)
+  const noir = new Noir(circuit, backend)
+  return noir
+}
+
 export async function verifyProof(proofType: ProofType, proof: ProofData) {
   const circuit = getCircuit(proofType)
   const backend = new BarretenbergBackend(circuit)
