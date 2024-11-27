48fe23c074db1029f4c08bb63680efc910e9c447
Slokh <slokhx@gmail.com>
2024-11-27 09:32:55 -0500

    0.39.0

--- packages/utils/src/proofs/verify.ts
+++ packages/utils/src/proofs/verify.ts
@@ -1,10 +1,11 @@
-import { BarretenbergBackend, ProofData } from '@noir-lang/backend_barretenberg'
+import { UltraPlonkBackend, ProofData } from '@aztec/bb.js'
 import { getCircuit } from './utils'
 import { ProofType } from './generate'
 
 export async function verifyProof(proofType: ProofType, proof: ProofData) {
   const circuit = getCircuit(proofType)
-  const backend = new BarretenbergBackend(circuit)
+  const backend = new UltraPlonkBackend(circuit.bytecode)
+  await backend.instantiate()
 
   return await backend.verifyProof(proof)
 }
