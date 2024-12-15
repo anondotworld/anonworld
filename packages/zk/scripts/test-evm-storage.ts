9ab9f6da22b7ed8a7e5d3cd008a17c37a5b6dea1
Kartik <slokhx@gmail.com>
2024-12-15 00:01:51 -0500

    fix: return values

--- packages/zk/scripts/test-evm-storage.ts
+++ packages/zk/scripts/test-evm-storage.ts
@@ -1,5 +1,5 @@
 import { evmStorage } from '../src'
-import { concat, createPublicClient, http, keccak256, pad, toHex, toRlp } from 'viem'
+import { concat, createPublicClient, http, keccak256, pad, toHex } from 'viem'
 import { base } from 'viem/chains'
 import { formatArray, formatHexArray } from './utils'
 
@@ -8,7 +8,8 @@ const client = createPublicClient({
   transport: http(),
 })
 
-const blockNumber = BigInt(23675000)
+const chainId = 8453
+const blockNumber = BigInt(23724083)
 const signature =
   '0x2d37b16631b67cbe79e8b115cda1ee74dde8492beef9fac0746777c463e0c8cc5cfd2cea5f1e2e6d8899e4fe33ab709a449e262cc9fc56c3d63b789d99270954'
 const messageHash = '0x9d447d956f18f06efc4e1fa2b715e6a46fe680d3d35e1ebe90b9d56ad1eddca1'
@@ -20,13 +21,13 @@ const balanceSlot = pad(toHex(0))
 
 async function main() {
   const storageKey = keccak256(concat([pad(address), balanceSlot]))
-  const proof = await client.getProof({
+  const ethProof = await client.getProof({
     address: tokenAddress,
     storageKeys: [storageKey],
     blockNumber: blockNumber,
   })
 
-  const storageProof = proof.storageProof[0]
+  const storageProof = ethProof.storageProof[0]
   const nodes = storageProof.proof.slice(0, storageProof.proof.length - 1)
   const leaf = storageProof.proof[storageProof.proof.length - 1]
 
@@ -36,9 +37,9 @@ async function main() {
     pub_key_x: formatHexArray(pubKeyX),
     pub_key_y: formatHexArray(pubKeyY),
     balance_slot: 0,
-    verified_balance: `0x${storageProof.value.toString(16)}`,
+    verified_balance: `0x${BigInt(1_000_000).toString(16)}`,
     storage_value: `0x${storageProof.value.toString(16)}`,
-    storage_root: formatHexArray(proof.storageHash),
+    storage_root: formatHexArray(ethProof.storageHash),
     storage_nodes: formatArray(nodes, (node) =>
       formatHexArray(node, { length: 532, pad: 'right' })
     ),
@@ -47,12 +48,29 @@ async function main() {
   }
 
   console.time('generateProof')
-  const proofData = await evmStorage.generate(input)
+  const proof = await evmStorage.generate(input)
   console.timeEnd('generateProof')
+
   console.time('verifyProof')
-  const verified = await evmStorage.verify(proofData)
+  const verified = await evmStorage.verify(proof)
   console.timeEnd('verifyProof')
-  console.log({ verified })
+
+  const storageHash = `0x${proof.publicInputs
+    .slice(0, 32)
+    .map((b) => BigInt(b).toString(16).padStart(2, '0'))
+    .join('')}`
+
+  const verifiedBalance = BigInt(proof.publicInputs[32]).toString()
+
+  console.log({
+    chainId,
+    blockNumber,
+    tokenAddress,
+    verifiedBalance,
+    storageHash,
+    verified,
+    proof,
+  })
 }
 
 main()
